bcd92b299549fff5f469db16ee795e1f8e63c79f
Gökay Atalay <i485542@fhict.nl>
2022-09-19 14:54:46 +0200

    Added Google Auth

--- workethic-fe/src/App.js
+++ workethic-fe/src/App.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const CLIENT_ID = '167499127647-vont3e759gg3g0902vig4c5vejd87cov.apps.googleusercontent.com';
+  
+  function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    return JSON.parse(jsonPayload);
+  }
+
+  async function handleCookie(data){
+    const response = await axios.post('http://127.0.0.1:8000/api/jwt', {'jwt':data});
+
+  }
+
+  async function handleJWT(credentialResponse) {
+    const credential = credentialResponse.credential;
+    const response = await handleCookie(credential);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +49,21 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <GoogleOAuthProvider clientId={CLIENT_ID}>
+        <GoogleLogin
+          buttonText="Login with Google"
+          isSignedIn={true}
+          useOneTap
+          auto_select
+          onSuccess={credentialResponse => {
+            handleJWT(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      </GoogleOAuthProvider>
     </div>
   );
 }
-
-export default App;
