cf24d005f3004fd4be44541cd0ef5855f6b12642
Gökay <tuhezhen@hotmail.com>
2022-11-24 09:16:08 +0100

    Fixed tests

--- workethic-fe/src/App.test.js
+++ workethic-fe/src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter, Router } from 'react-router-dom';
 import App from './App';
 import Login from './Pages/Login';
 
 test('renders login in nav', () => {
-  render(<Login />);
+  render(<BrowserRouter><Login/></BrowserRouter>);
   const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
--- workethic-fe/src/Pages/Login.jsx
+++ workethic-fe/src/Pages/Login.jsx
@@ -10,6 +10,7 @@ export default function Login() {
   return (
     <>
       <div className='w-full h-[80vh] flex justify-center items-center'>
+        <h2>Login</h2>
         <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
           <LoginButton value={value}/>
         </GoogleOAuthProvider>
