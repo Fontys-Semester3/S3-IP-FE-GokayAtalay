2c2c6c7f70920e5704df3a3b1ff9f8983b2e1bf5
Gökay <tuhezhen@hotmail.com>
2022-11-29 23:00:31 +0100

    Update UserSession.test.js

--- workethic-fe/src/Tests/UserSession.test.js
+++ workethic-fe/src/Tests/UserSession.test.js
@@ -1,11 +1,6 @@
-import { render, screen, waitFor, cleanup } from '@testing-library/react';
-import { BrowserRouter, Router } from 'react-router-dom';
-import { createMemoryHistory } from '@remix-run/router';
+import { render, screen, cleanup, } from '@testing-library/react';
 import UserService from '../Services/UserService';
 import Navbar from '../Components/Navbar';
-import Tasks from '../Pages/Tasks';
-import App from '../App';
-
 afterEach(cleanup);
 
 test('no user in session', () => {
@@ -28,10 +23,20 @@ test('navbar shows tasks after login', () => {
     render(<Navbar/>);
     expect(screen.getByText('Tasks')).toBeInTheDocument();
 })
+
   
 test('user in session', () => {
     const service = new UserService();
     const user = service.setUserSession({name: 'Gökay', sub: '97gh9gn0ih09u6ojhotyij956', email: 'gokayatalay@gmail.com'});
   
     expect(user).not.toBeNull();
+})
+
+test('user not in session after logout', () => {
+    const service = new UserService();
+    service.setUserSession({name: 'Gökay', sub: '97gh9gn0ih09u6ojhotyij956', email: 'gokayatalay@gmail.com'});
+    service.deleteUserSession();
+
+    const user = service.getUserSession();
+    expect(user).toBeNull();
 })
